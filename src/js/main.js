02eaeccbf9b996eb058e1131c8256c9665384226
unknown <C:\Users\\AppData\Roaming\The Bat!>
2023-06-29 18:09:19 +0300

    work begin

--- src/js/main.js
+++ src/js/main.js
@@ -247,3 +247,24 @@ if (document.querySelector('.we-offer')) {
 }
 
 /*accordion end*/
+
+/* portfolio-tabs */
+
+const portfolioTabsNav = document.querySelector('.portfolio-tabs-nav');
+
+portfolioTabsNav.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.classList.contains('portfolio-tabs-nav__btn')) {
+    console.log('asd');
+
+    const path = target.dataset.path;
+
+    document.querySelectorAll('.portfolio-tabs__item').forEach(el => { el.style.display = 'none' });
+
+    document.querySelectorAll('[data-target="${path}"]').forEach(el => { 
+      el.closest('.portfolio-tabs__item').classList.add('portfolio-tabs__item--visible');
+    });
+  }
+});
+
+/* portfolio-tabs end */
